cdd73165f40079661a12abcb58d69fb9dae25cf4
Olu <a00287691@mycambrian.ca>
2023-09-26 18:41:25 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kotlin1st"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/kotlin1st/Greeting.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin1st
+import kotlin.random.Random
+
+class Greeting(listOf: List<String>) {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): List<String> = buildList {
+        add(if (Random.nextBoolean()) "Hi!" else "Hello!")
+        add("Guess what it is! > ${platform.name.reversed()}!")
+    }
+}
